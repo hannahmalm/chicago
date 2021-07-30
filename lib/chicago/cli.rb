54099eb443705ce7e7f6b5c72329b4a5039ed3fc
Hannah Malm <hannahemalm616@gmail.com>
2021-07-29 21:44:55 -0400

    new logic

--- lib/chicago/cli.rb
+++ lib/chicago/cli.rb
@@ -10,14 +10,18 @@ class Chicago::CLI
         puts 'Welcome to Chicago Activities!'
         #print out a list of Categories - list_categories
         list_categories
-        
         #get user input on which category to choose - get_user_category_input
         get_user_category_input
         #get activity for respective cateogry - list_activities
-        # list_activities
+        # list_activities - this happens after the user category input
         #get user input on which activity they want more information on - get_user_activity_input
+        get_user_activity_input
         #print out the name, description, category, and url for the activity - then ask user if they want more input
-        
+        #details
+        #Do you want to see another activity? - y/n - if yes loop back 
+        #new_activty
+        #end the CLI
+        #goodbye
     end 
 
     #dont need to scrape this because categories wont change
@@ -36,7 +40,6 @@ class Chicago::CLI
         print "Please choose a number associated with a category you would like to see activities for: 
         
         "
-
         input = gets.to_i
             if(input >= 1 && input <=6) 
               list_activities
@@ -48,9 +51,48 @@ class Chicago::CLI
     def list_activities
         puts "The following is a list of activities for category:"
         #add in scraper data
-        puts "Fake Data"
+        @activities = ["Shopping at Maceys", "Running Club", "Justin Bieber concert"]
+        @activities.each.with_index(1) {|index, activity|
+            puts "#{activity} - #{index}"
+        }
+        puts "Which activity are you interested in learning more about? Please select a number: "
+    end 
+
+
+    def get_user_activity_input
+        input = gets.to_i
+        if(input >= 1 && input <=100) 
+         puts details
+        else
+         puts "Please choose an activity or type Exit to exit"
+        end 
+    end
+
+    def details
+        puts "Activty Name: "
+        puts "Activity Details: "
+        puts "URL: "
+
+        new_activty
+    end 
+
+    def new_activty
+        puts "Do you want to see another activity?"
+        input = gets.chomp.to_s 
+        if input == "Y" || input == "Yes" || input == "y"
+            #loop back to the activity method to start over 
+            list_categories
+            get_user_category_input
+        elsif input == "N" || input == "No" || input == "n"
+            goodbye 
+        else 
+            "Please enter Yes or No"
+        end
+    end 
+
+
+    def goodbye
+        puts "Thank You for using Chicago Activity Tracker"
     end 
  
-
-    
 end 
